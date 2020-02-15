eddee232f8291acf504074a73be7fba1d039ca12
Roald Ignacio <roaldjap@gmail.com>
2020-02-15 13:34:34 +1300

    Added axios and communicate it with react

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import Ticket from './Ticket';
-import sampleTickets from '../sample-data.json';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import axios from 'axios';
+// local data here
+// import sampleTickets from '../sample-data.json';
 
 class App extends React.Component {
 	constructor(props) {
@@ -16,22 +18,35 @@ class App extends React.Component {
 	};
 
 	loadSampleTickets = () => {
-		const tickets = sampleTickets.map(data => {
-			return data;
-		});
-		this.setState({ tickets: tickets });
+		// get the data from sample-data.json
+		// const tickets = sampleTickets.map(data => {
+		// 	return data;
+		// });
+
+		// this.setState({ tickets: tickets });
+
+		var self = this;
+		axios
+			.get('https://frontend-navigatetravel.ntstage.com/api')
+			.then(function(response) {
+				console.log(response);
+				self.setState({ tickets: response.data });
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 	};
 
 	componentDidMount = () => {
 		this.loadSampleTickets();
-  };
-  
-  scrollingRight = () => {
-    this._rowContent.scrollLeft += 50;
-  }
+	};
+
+	scrollingRight = () => {
+		this._rowContent.scrollLeft += 50;
+	};
 
 	render() {
-    var self = this;
+		var self = this;
 		return (
 			<div className="App">
 				<Grid>
@@ -41,12 +56,20 @@ class App extends React.Component {
 						</Col>
 						<Col md={9}>
 							<div className="content-wrapper">
-                <button className="scroller-right" onClick={this.scrollingRight}> &#9658; </button> 
-								<div className="row-inline-block"  ref={function (el) {self._rowContent = el}}>
+								<button className="scroller-right" onClick={this.scrollingRight}>
+									{' '}
+									&#9658;{' '}
+								</button>
+								<div
+									className="row-inline-block"
+									ref={function(el) {
+										self._rowContent = el;
+									}}
+								>
 									{Object.keys(this.state.tickets).map(key => (
 										<Ticket key={key} details={this.state.tickets[key]} />
 									))}
-                </div>
+								</div>
 							</div>
 						</Col>
 					</Row>
--- src/Components/Ticket.js
+++ src/Components/Ticket.js
@@ -1,114 +1,113 @@
-import React, { Component } from 'react'
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import React, { Component } from 'react';
 
 class Ticket extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props);
 
-    this.state = {
-         
-    }
-  }
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <div class="ticket-wrapper">
-          <div className="header">
-            {this.props.details.dateText}<br/>
-            ({this.props.details.dateDay})
-          </div>
-          <div className="red-badge">
-            {this.props.details.temperature}
-          </div>
+	render() {
+		return (
+			<div className="ticket-wrapper">
+				<div className="header">
+					{this.props.details.dateText}
+					<br />({this.props.details.dateDay})
+				</div>
+				<div className="red-badge">{this.props.details.temperature}</div>
 
-          {/* Premier */}
-          <div className="ticket">
-            <div className="ticket-header">
-            {this.props.details.products[0].priceWithDiscount != null ? (
-              <React.Fragment>
-                <p className="orig-price">{this.props.details.products[0].price}</p>
-                <p className="last-price">{this.props.details.products[0].priceWithDiscount}</p>
-              </React.Fragment>
-            ) : (
-              <p className="last-price no-discount">{this.props.details.products[0].price}</p>
-            )}
-            </div>
-            <div className={`rip ${this.props.details.products[0].status === "Sold Out" ? "hide-bottom" : ""}`}></div>
-            {this.props.details.products[0].status === "Last Spaces" ? (
-              <div className="ticket-footer last-spaces">
-                <p class="status">{this.props.details.products[0].status}</p>
-              </div>
-            ) : this.props.details.products[0].status === "Sold Out" ?  (
-              <div className="ticket-footer sold-out">
-                <p class="status">{this.props.details.products[0].status}</p>
-              </div>
-            ) : (
-              <div className="ticket-footer">
-                <p class="status">{this.props.details.products[0].status}</p>
-              </div>
-            ) }
-          </div>
+				{/* Premier */}
+				<div className="ticket">
+					<div className="ticket-header">
+						{this.props.details.products[0].priceWithDiscount != null ? (
+							<React.Fragment>
+								<p className="orig-price">{this.props.details.products[0].price}</p>
+								<p className="last-price">{this.props.details.products[0].priceWithDiscount}</p>
+							</React.Fragment>
+						) : (
+							<p className="last-price no-discount">{this.props.details.products[0].price}</p>
+						)}
+					</div>
+					<div
+						className={`rip ${this.props.details.products[0].status === 'Sold Out' ? 'hide-bottom' : ''}`}
+					></div>
+					{this.props.details.products[0].status === 'Last Spaces' ? (
+						<div className="ticket-footer last-spaces">
+							<p className="status">{this.props.details.products[0].status}</p>
+						</div>
+					) : this.props.details.products[0].status === 'Sold Out' ? (
+						<div className="ticket-footer sold-out">
+							<p className="status">{this.props.details.products[0].status}</p>
+						</div>
+					) : (
+						<div className="ticket-footer">
+							<p className="status">{this.props.details.products[0].status}</p>
+						</div>
+					)}
+				</div>
 
-          {/* Premier PLus */}
-          <div className="ticket">
-            <div className="ticket-header">
-            {this.props.details.products[1].priceWithDiscount != null ? (
-              <React.Fragment>
-                <p className="orig-price">{this.props.details.products[1].price}</p>
-                <p className="last-price">{this.props.details.products[1].priceWithDiscount}</p>
-              </React.Fragment>
-            ) : (
-              <p className="last-price no-discount">{this.props.details.products[1].price}</p>
-            )}
-            </div>
-            <div className={`rip ${this.props.details.products[1].status === "Sold Out" ? "hide-bottom" : ""}`}></div>
-            {this.props.details.products[1].status === "Last Spaces" ? (
-              <div className="ticket-footer last-spaces">
-                <p class="status">{this.props.details.products[1].status}</p>
-              </div>
-            ) : this.props.details.products[1].status === "Sold Out" ?  (
-              <div className="ticket-footer sold-out">
-                <p class="status">{this.props.details.products[1].status}</p>
-              </div>
-            ) : (
-              <div className="ticket-footer">
-                <p class="status">{this.props.details.products[1].status}</p>
-              </div>
-            ) }
-          </div>
+				{/* Premier PLus */}
+				<div className="ticket">
+					<div className="ticket-header">
+						{this.props.details.products[1].priceWithDiscount != null ? (
+							<React.Fragment>
+								<p className="orig-price">{this.props.details.products[1].price}</p>
+								<p className="last-price">{this.props.details.products[1].priceWithDiscount}</p>
+							</React.Fragment>
+						) : (
+							<p className="last-price no-discount">{this.props.details.products[1].price}</p>
+						)}
+					</div>
+					<div
+						className={`rip ${this.props.details.products[1].status === 'Sold Out' ? 'hide-bottom' : ''}`}
+					></div>
+					{this.props.details.products[1].status === 'Last Spaces' ? (
+						<div className="ticket-footer last-spaces">
+							<p className="status">{this.props.details.products[1].status}</p>
+						</div>
+					) : this.props.details.products[1].status === 'Sold Out' ? (
+						<div className="ticket-footer sold-out">
+							<p className="status">{this.props.details.products[1].status}</p>
+						</div>
+					) : (
+						<div className="ticket-footer">
+							<p className="status">{this.props.details.products[1].status}</p>
+						</div>
+					)}
+				</div>
 
-          {/* Catamaran */}
-          <div className="ticket">
-            <div className="ticket-header">
-            {this.props.details.products[2].priceWithDiscount != null ? (
-              <React.Fragment>
-                <p className="orig-price">{this.props.details.products[2].price}</p>
-                <p className="last-price">{this.props.details.products[2].priceWithDiscount}</p>
-              </React.Fragment>
-            ) : (
-              <p className="last-price no-discount">{this.props.details.products[2].price}</p>
-            )}
-            </div>
-            <div className={`rip ${this.props.details.products[2].status === "Sold Out" ? "hide-bottom" : ""}`}></div>
-            {this.props.details.products[2].status === "Last Spaces" ? (
-              <div className="ticket-footer last-spaces">
-                <p class="status">{this.props.details.products[2].status}</p>
-              </div>
-            ) : this.props.details.products[2].status === "Sold Out" ?  (
-              <div className="ticket-footer sold-out">
-                <p class="status">{this.props.details.products[2].status}</p>
-              </div>
-            ) : (
-              <div className="ticket-footer">
-                <p class="status">{this.props.details.products[2].status}</p>
-              </div>
-            ) }
-          </div>
-
-          
-      </div>
-    )
-  }
+				{/* Catamaran */}
+				<div className="ticket">
+					<div className="ticket-header">
+						{this.props.details.products[2].priceWithDiscount != null ? (
+							<React.Fragment>
+								<p className="orig-price">{this.props.details.products[2].price}</p>
+								<p className="last-price">{this.props.details.products[2].priceWithDiscount}</p>
+							</React.Fragment>
+						) : (
+							<p className="last-price no-discount">{this.props.details.products[2].price}</p>
+						)}
+					</div>
+					<div
+						className={`rip ${this.props.details.products[2].status === 'Sold Out' ? 'hide-bottom' : ''}`}
+					></div>
+					{this.props.details.products[2].status === 'Last Spaces' ? (
+						<div className="ticket-footer last-spaces">
+							<p className="status">{this.props.details.products[2].status}</p>
+						</div>
+					) : this.props.details.products[2].status === 'Sold Out' ? (
+						<div className="ticket-footer sold-out">
+							<p className="status">{this.props.details.products[2].status}</p>
+						</div>
+					) : (
+						<div className="ticket-footer">
+							<p className="status">{this.props.details.products[2].status}</p>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Ticket
+export default Ticket;
